53862179b96fea235cec50403b3ba413fb3869eb
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-09-05 14:55:38 -0300

    Criando o IndexController

---
+++ src/main/java/io/luaprogrammer/carros/api/rest/IndexController.java
@@ -0,0 +1,15 @@
+package io.luaprogrammer.carros.api.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class IndexController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
+}

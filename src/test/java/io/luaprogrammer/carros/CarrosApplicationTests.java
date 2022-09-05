246a590c00cac9046365005a0958b967f09e3b19
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-09-05 14:47:00 -0300

    Subindo aplicação 1.0

---
+++ src/test/java/io/luaprogrammer/carros/CarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.luaprogrammer.carros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

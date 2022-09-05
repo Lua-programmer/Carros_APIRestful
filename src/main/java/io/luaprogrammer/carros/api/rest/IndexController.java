f66caa948fb55854cf0c3a0b065d022e6cf5be0a
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-09-05 15:43:58 -0300

    GET - @RequestParam

--- src/main/java/io/luaprogrammer/carros/api/rest/IndexController.java
+++ src/main/java/io/luaprogrammer/carros/api/rest/IndexController.java
@@ -2,6 +2,7 @@ package io.luaprogrammer.carros.api.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/")
@@ -12,4 +13,11 @@ public class IndexController {
     public String hello() {
         return "Hello World";
     }
+
+    @GetMapping("/login")
+    public String login(@RequestParam("login") String login, @RequestParam("senha") String senha) {
+        return "login = " + login + " senha = " + senha;
+    }
+
+    //localhost:8080/login?login=Luana&senha=123  para colocar o primeiro parametro para o "?" e os demais "&"
 }
